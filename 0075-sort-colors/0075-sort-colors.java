bc46f568567daa6402122251219745a372be896c
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-03-27 19:26:11 +0530

    Time: 1 ms (22.74%), Space: 40.9 MB (60.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,31 +2,13 @@ import java.util.Arrays;
 class Solution {
     public void sortColors(int[] nums){
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==2){
-                for(int j=nums.length-1;j>i;j--){
-                    if(nums[j]!=2){
-                        int a=nums[j];
-                        nums[j]=nums[i];
-                        nums[i]=a;
-                        System.out.println(Arrays.toString(nums));
-                        break;
-                    }
+            for(int j=0;j<nums.length-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int a=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=a;
                 }
             }
-            if(nums[i]==0){
-                for(int j=0;j<i;j++){
-
-                    if(nums[j]!=0){
-                        int a=nums[j];
-                        nums[j]=nums[i];
-                        nums[i]=a;
-                        System.out.println(Arrays.toString(nums));
-                        break;
-                    }
-
-                }
-            }
-
         }
     }
 }
