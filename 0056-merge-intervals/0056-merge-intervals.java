4a8962ad1d01c2dec48720121d15d830fd83f2ba
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-04-06 22:52:56 +0530

    Time: 20 ms (6.62%), Space: 47.8 MB (16.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Double.compare(a[0], b[0]));
-        for (int[] row : intervals)
-            System.out.print(Arrays.toString(row));
         ArrayList<ArrayList<Integer> > x = new ArrayList<ArrayList<Integer> >();
         x.add(new ArrayList<Integer>());
         x.get(0).addAll(Arrays.asList(intervals[0][0],intervals[0][1]));
         int a=0;
         int max=x.get(0).get(1);
-        System.out.println(x.get(a));
         for(int i=1;i<intervals.length;i++){
             if(max>=intervals[i][0]){
                 max=Math.max(intervals[i][1],max);
-                System.out.println("max= "+max);
+                //System.out.println("max= "+max);
                 x.get(a).set(1,max);
-                System.out.println(x.get(a));
+               // System.out.println(x.get(a));
             }
             else{
                 a++;
                 x.add(new ArrayList<Integer>());
                 x.get(a).addAll(Arrays.asList(intervals[i][0],intervals[i][1]));
                 max=intervals[i][1];
-                System.out.println(x.get(a));
+               // System.out.println(x.get(a));
             }
         }
         int[][] arr = x.stream().map(l -> l.stream().mapToInt(Integer::intValue).toArray()).toArray(int[][]::new);
