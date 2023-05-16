0b0f442fc811b030b875bb0c14e3a6c22ad90149
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-05-16 08:40:48 +0530

    Time: 2 ms (41.01%) | Memory: 43.3 MB (8.25%) - LeetSync

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -2,10 +2,10 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans= new ArrayList<>();
         List<Integer> list= new ArrayList<Integer>();
-        permutation(nums,0,list,ans);
+        permutation(nums,list,ans);
         return ans;
     }
-    static void permutation(int[] nums,int ind,List<Integer> list,List<List<Integer>> ans){
+    static void permutation(int[] nums,List<Integer> list,List<List<Integer>> ans){
         if(list.size()==nums.length){
             ans.add(new ArrayList<>(list));
             return;
@@ -13,7 +13,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(!list.contains(nums[i])){
                 list.add(nums[i]);
-                permutation(nums,i+1,list,ans);
+                permutation(nums,list,ans);
                 list.remove(list.size()-1);
             }
         }
