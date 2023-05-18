6448f2958e0d9389e5076afc11389110a17b2889
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-05-18 15:55:11 +0530

    Time: 12 ms (79.04%) | Memory: 55.8 MB (77.55%) - LeetSync

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length==1){
-            return false;
-        }
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
+        Set<Integer> set =new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+            }
+            else{
                 return true;
             }
         }
