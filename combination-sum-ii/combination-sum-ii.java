e0ef32dbc93d7148f411d82c9a06af7492007fd0
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-05-18 08:51:28 +0530

    Time: 3 ms (66.08%) | Memory: 43 MB (52.92%) - LeetSync

---
+++ combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> arr = new ArrayList<>();
+        List<Integer> flag=new ArrayList<>();
+        Arrays.sort(candidates);
+        //System.out.println(Arrays.toString(candidates));
+        combination(candidates,target,arr,0,flag);
+        return arr;
+    }
+    static void combination(int[] candidates, int target,List<List<Integer>> arr,int ind,List<Integer> flag){
+        if(target==0){
+            //List<Integer> dummy=new ArrayList<>(flag);
+            //Collections.copy(dummy, flag);
+            //Collections.sort(dummy);
+            if(!arr.contains(flag)){
+                arr.add(new ArrayList<>(flag));
+            }
+            return;
+        }
+        for(int i = ind;i<candidates.length;i++){
+            if(i != ind && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            if(candidates[i] > target){break;}
+            flag.add(candidates[i]);
+            combination(candidates,target-candidates[i],arr,i+1,flag);
+            flag.remove(flag.size()-1);
+        }
+    }
+}
