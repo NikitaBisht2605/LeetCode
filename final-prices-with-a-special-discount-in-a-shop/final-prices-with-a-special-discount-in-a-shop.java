b10126d45f132462ed71c0bc96cbdcd38e4e5eab
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-06-11 23:13:12 +0530

    Time: 4 ms (42.43%) | Memory: 43.9 MB (14.68%) - LeetSync

--- final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,37 +1,21 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        Stack<Integer> stack=new Stack<>();
-        int[] nsr=new int[prices.length];
-        for(int i=prices.length-1;i>=0;i--){
-            if(stack.isEmpty()){
-                stack.push(prices[i]);
-                nsr[i]=0;
+        int n = prices.length;
+        Stack <Integer> s = new Stack <>();
+        int ans[] = new int [n];
+        for(int i=n-1; i>=0; i--){
+            while(!s.isEmpty() && prices[i] < prices[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                ans[i] = prices[i];
             }
             else{
-                if(stack.peek()<=prices[i]){
-                    nsr[i]=stack.peek();
-                    stack.push(prices[i]);
-                }
-                else{
-                    while(!stack.isEmpty()){
-                        if(stack.peek()<=prices[i]){
-                            nsr[i]=stack.peek();
-                            stack.push(prices[i]);
-                            break;
-                        }
-                        stack.pop();
-                    }
-                    stack.push(prices[i]);
-                    if(stack.isEmpty()){
-                        stack.push(prices[i]);
-                        nsr[i]=0;
-                    }
-                }
+                int min = prices[i]- prices[s.peek()];
+                ans[i]= min;
             }
+            s.push(i);
         }
-        for(int i=0;i<prices.length;i++){
-            nsr[i]=prices[i]-nsr[i];
-        }
-        return nsr;
+        return ans;
     }
 }
