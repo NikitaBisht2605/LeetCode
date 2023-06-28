259d62cf8e228e72dc66abe169af4e241f275870
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-06-28 11:27:54 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (25.46%) - LeetSync

--- count-complete-tree-nodes/count-complete-tree-nodes.java
+++ count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,25 +15,9 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if(root==null){
+        if(root == null) {
             return 0;
         }
-        Queue<TreeNode> q=new LinkedList<TreeNode>();
-        q.offer(root);
-        int ans=0;
-        while(!q.isEmpty()){
-            int len=q.size();
-            ans=ans+len;
-            for(int i=0;i<len;i++){
-                if(q.peek().left!=null){
-                    q.offer(q.peek().left);
-                }
-                if(q.peek().right!=null){
-                    q.offer(q.peek().right);
-                }
-                q.poll();
-            }
-        }
-        return ans;
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
