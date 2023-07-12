b455c0b48036f8cb4bc1d8bbfe8942cd72d80f23
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-07-12 18:44:08 +0530

    Time: 19 ms (43.14%) | Memory: 40.4 MB (23.10%) - LeetSync

--- sudoku-solver/sudoku-solver.java
+++ sudoku-solver/sudoku-solver.java
@@ -1,31 +1,19 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        fillNum(board);
+        solve(board);
     }
-    static boolean check(char[][] board,int row, int col,char c){
-        //Checking vetically and horizontally
-        for(int i=0;i<board.length;i++){
-            if(board[row][i]==c || board[i][col]==c){
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c){
-                return false;
-            }
-        }
-        return true;      
-    }
-    static boolean fillNum(char[][] board){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                if(board[i][j]=='.'){
+    static boolean solve(char[][] board){
+        for(int row=0;row<board.length;row++){
+            for(int col=0;col<board.length;col++){
+                if(board[row][col]=='.'){
                     for(char c='1';c<='9';c++){
-                        if(check(board,i,j,c)){
-                            board[i][j]=c;
-                            if(fillNum(board)){
+                        if(validate(row,col,c,board)){
+                            board[row][col]=c;
+                            if(solve(board)){
                                 return true;
                             }
                             else{
-                                board[i][j]='.';
+                                board[row][col]='.';
                             }
                         }
                     }
@@ -35,4 +23,19 @@ class Solution {
         }
         return true;
     }
+    static boolean validate(int row, int col,char c,char[][] board){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c){
+                return false;
+            }
+            if(board[i][col]==c){
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
