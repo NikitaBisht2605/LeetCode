e2a80d3e9f2f424912fdb506508047161c2215bd
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-07-13 07:10:58 +0530

    Time: 11 ms (56.87%) | Memory: 55.2 MB (77.02%) - LeetSync

--- palindrome-partitioning/palindrome-partitioning.java
+++ palindrome-partitioning/palindrome-partitioning.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans= new ArrayList<>();
-        List<String> flag=new ArrayList<>();
-        palindrome(0,ans,flag,s);
-        return ans;
+        List<List<String>> res=new ArrayList<>();
+        List<String> list=new ArrayList<>();
+        recursion(0,list,res,s);
+        return res;
     }
-    static void palindrome(int ind,List<List<String>> ans,List<String> flag,String s){
-        if(s.length()==ind){
-            ans.add(new ArrayList<>(flag));
+    static void recursion(int ind,List<String> list,List<List<String>> res,String s){
+        if(ind==s.length()){
+            res.add(new ArrayList<>(list));
             return;
         }
         for(int i=ind;i<s.length();i++){
-            if(check(s,ind,i)){
-                flag.add(s.substring(ind,i+1));
-                palindrome(i+1,ans,flag,s);
-                flag.remove(flag.size()-1);
+            if(isPalindrome(ind,i,s)){
+                list.add(s.substring(ind,i+1));
+                recursion(i+1,list,res,s);
+                list.remove(list.size()-1);
             }
         }
     }
-    static boolean check(String s,int j,int i){
-        while(j<=i){
-            if(s.charAt(j)!=s.charAt(i)){
+    static boolean isPalindrome(int ind, int i,String s){
+        while(ind<=i){
+            if(s.charAt(ind)!=s.charAt(i)){
                 return false;
             }
-            j++;
             i--;
+            ind++;
         }
         return true;
     }
