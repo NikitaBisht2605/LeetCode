b2bba45569be455517dc4fd94cadf0027e70418a
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-07-13 19:00:45 +0530

    Time: 589 ms (5.11%) | Memory: 55.2 MB (84.67%) - LeetSync

---
+++ maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        int[] nsl=new int[nums.length];
+        int[] nsr=new int[nums.length];
+        Stack<Integer> s=new Stack<Integer>();
+        for(int n=nums.length-1;n>=0;n--){
+            while(s.isEmpty()==false && nums[n]<=nums[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[n]=nums.length;
+            }
+            else{
+                nsr[n]=s.peek();
+            }
+            s.push(n);
+        }
+        while(s.isEmpty()==false){
+            s.pop();
+        }
+
+        for(int n=0;n<nums.length;n++){
+            while(s.isEmpty()==false && nums[n]<=nums[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[n]=-1;
+            }
+            else{
+                nsl[n]=s.peek();
+            }
+            s.push(n);
+        }
+
+        int ans=0;
+        System.out.println(Arrays.toString(nsr));
+        System.out.println(Arrays.toString(nsl));
+        for(int i=0;i<nums.length;i++){
+            if(nsl[i]+1<=k && nsr[i]-1>=k){
+                ans=Math.max(ans,nums[i]*(nsr[i]-nsl[i]-1));
+            }
+        }
+        return ans;
+    }
+}
