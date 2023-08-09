d421b667831b6c1d8d1a9d3f113cccb9664e690b
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2023-08-09 18:31:08 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
@@ -115,6 +115,7 @@ class GfG {
 
 
 //User function Template for Java
+
 class Tuple{
     Node node;
     int row;
@@ -125,14 +126,15 @@ class Tuple{
         col=_col;
     }
 }
-
 class Solution
 {
     //Function to return a list containing the bottom view of the given tree.
     public ArrayList <Integer> bottomView(Node root)
     {
         // Code here
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map= new TreeMap<>();
+        
+        ArrayList<Integer> list=new ArrayList<>();
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap<>();
         Queue<Tuple> q=new LinkedList<Tuple>();
         q.offer(new Tuple(root,0,0));
         while(!q.isEmpty()){
@@ -143,10 +145,12 @@ class Solution
             if(!map.containsKey(x)){
                 map.put(x,new TreeMap<>());
             }
-            else{
+            else if(map.containsKey(x)){
                 map.get(x).clear();
+                map.get(x).put(y,new PriorityQueue<>());
+                map.get(x).get(y).offer(node.data);
             }
-            map.get(x).put(y,new PriorityQueue<Integer>());
+            map.get(x).put(y,new PriorityQueue<>());
             map.get(x).get(y).offer(node.data);
             if(node.left!=null){
                 q.offer(new Tuple(node.left,x-1,y+1));
@@ -155,12 +159,11 @@ class Solution
                 q.offer(new Tuple(node.right,x+1,y+1));
             }
         }
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(TreeMap<Integer,PriorityQueue<Integer>> tree:map.values()){
-            for(PriorityQueue<Integer> pq:tree.values()){
-                arr.add(pq.poll());
+        for(TreeMap<Integer,PriorityQueue<Integer>> mp:map.values()){
+            for(PriorityQueue<Integer> pq:mp.values()){
+                list.add(pq.poll());
             }
         }
-        return arr;
+        return list;
     }
 }
